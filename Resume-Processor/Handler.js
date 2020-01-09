e072ad7b6274724b8926bfb743fb1183a0b13650
SpericBlockchain <mekhakrishnan@spericorn.com>
2020-01-09 15:02:58 +0530

    Fix 1

--- Resume-Processor/Handler.js
+++ Resume-Processor/Handler.js
@@ -1,60 +1,58 @@
-'use strict'
+"use strict";
 
-const { TransactionHandler } = require('sawtooth-sdk/processor/handler'),
+const { TransactionHandler } = require("sawtooth-sdk/processor/handler"),
 	{
 		InvalidTransaction,
 		InternalError
-	} = require('sawtooth-sdk/processor/exceptions'),
-	crypto = require('crypto'),
-	{ TextEncoder, TextDecoder } = require('text-encoding/lib/encoding')
+	} = require("sawtooth-sdk/processor/exceptions"),
+	crypto = require("crypto"),
+	{ TextEncoder, TextDecoder } = require("text-encoding/lib/encoding")
 
-const encoder = new TextEncoder('utf8'),
-	decoder = new TextDecoder('utf8')
+const encoder = new TextEncoder("utf8"),
+	decoder = new TextDecoder("utf8");
 
 function hash(data) {
 	return crypto
-		.createHash('sha512')
+		.createHash("sha512")
 		.update(data)
-		.digest('hex')
+		.digest("hex");
 }
 
-const FAMILY_NAME = 'resume',
-	NAMESPACE = hash(FAMILY_NAME).substring(0, 6)
+const FAMILY_NAME = "resume",
+	NAMESPACE = hash(FAMILY_NAME).substring(0, 6);
 
 function getAddress(publicKey) {
 	const keyHash = hash(publicKey),
-		nameHash = NAMESPACE + keyHash.slice(0, 64)
-	return nameHash
+		nameHash = NAMESPACE + keyHash.slice(0, 64);
+	return nameHash;
 }
 
 async function AddResume(context, address, data) {
-	const state = await context.getState([address])
+	const state = await context.getState([address]);
 		const dataBytes = encoder.encode(JSON.stringify([data])),
 			entries = {
 				[address]: dataBytes
-			}
-		const Status = await context.setState(entries)
-		console.log('TCL: AddResume -> Status', Status)
-		context.addReceiptData(Buffer.from(Status, 'utf8'))
-		return Status
+			};
+		const Status = await context.setState(entries);
+		context.addReceiptData(Buffer.from(Status, "utf8"));
+		return Status;
 }
 
 async function UpdateResume(context, address, data) {
-	console.log('Log: AddImage -> data', data)
 	const dataBytes = encoder.encode(JSON.stringify([data])),
 		entries = {
 			[address]: dataBytes
-		}
-	const Status = await context.setState(entries)
-	context.addReceiptData(Buffer.from(Status, 'utf8'))
-	return Status
+		};
+	const Status = await context.setState(entries);
+	context.addReceiptData(Buffer.from(Status, "utf8"));
+	return Status;
 }
 
 //transaction handler class
 
 class Handler extends TransactionHandler {
 	constructor() {
-		super(FAMILY_NAME, ['1.0'], [NAMESPACE])
+		super(FAMILY_NAME, ["1.0"], [NAMESPACE])
 	}
 	//apply function
 	apply(transactionProcessRequest, context) {
@@ -64,19 +62,17 @@ class Handler extends TransactionHandler {
 				PayloadBytes = decoder.decode(transactionProcessRequest.payload),
 				Payload = JSON.parse(PayloadBytes),
 				address = getAddress(userPublicKey),
-				action = Payload[0]
+				action = Payload[0];
 
-			console.log('TCL: Handler -> apply -> Payload', Payload)
 			if (action === 1) {
-				return AddResume(context, address, Payload[1])
+				return AddResume(context, address, Payload[1]);
 			} else if (action === 2) {
-				return UpdateResume(context, address, Payload[1])
+				return UpdateResume(context, address, Payload[1]);
 			}
 		} catch (err) {
-			console.log('TCL: Handler -> apply -> err', err)
 			// throw new InternalError(err)
 		}
 	}
 }
 
-module.exports = Handler
+module.exports = Handler;
--- Resume-Processor/Processor.js
+++ Resume-Processor/Processor.js
@@ -1,15 +1,14 @@
-'use strict'
+"use strict";
 //works in strict mode
-const { TransactionProcessor } = require('sawtooth-sdk/processor')
-const Handler = require('./Handler')
+const { TransactionProcessor } = require("sawtooth-sdk/processor");
+const Handler = require("./Handler");
 
 if (process.argv.length < 3) {
-	console.log('missing a validator address')
-	process.exit(1)
+	process.exit(1);
 }
 
-const address = process.argv[2]
+const address = process.argv[2];
 
-const transactionProcessor = new TransactionProcessor(address)
-transactionProcessor.addHandler(new Handler())
-transactionProcessor.start()
+const transactionProcessor = new TransactionProcessor(address);
+transactionProcessor.addHandler(new Handler());
+transactionProcessor.start();
